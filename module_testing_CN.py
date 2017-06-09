634be8c863217c54e1231cf6b6f3175243f17f96
ccnavio <carie.navio@gmail.com>
2017-06-09 11:17:27 -0400

    Carie's first commit

---
+++ module_testing_CN.py
@@ -0,0 +1,13 @@
+"""
+
+MAVProxy Testing Multicopter Flight
+Carie Navio
+Rev. 1
+
+"""
+
+import os, time, platform, math
+from pymavlink import mavutil
+from MAVProxy.modules.lib import mp_util 
+
+
