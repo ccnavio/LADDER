2b1e1fb9e7fc510b7f7befc5a6a38320c4314648
HerbertHoover <srich046@odu.edu>
2017-06-14 13:57:30 -0400

    Added a sample for arming copters
    Could be wayyyy off when it comes to the incoming signals

--- module_testing_CN.py
+++ module_testing_CN.py
@@ -23,13 +23,24 @@ class CmdTestModule(mp_module.MPModule):
 	super(CmdlongModule, self).__init__(mpstate, "cmdlong")
 	"""Commads for CmdTestmodule"""
 
-	def armCopters(self, args):
-		if( args == 1 )
-			# arm 1 copter
-		elif( args == 2 )
-			# arm 2 copters
-		elif( args == 3 )
-			# arm 3 copters
+	# Method to arm the three copters
+	# Doesn't work if you have less than 3 copters 
+	def armCopters(self, coptersingal):
+		# Set copters to a default of False or "off"
+		self.copter1 = 0, self.copter2 = 0, self.copter3 = 0
+		
+		while( True ):
+			# Stop once all signals have been received
+			if( self.copter1 && self.copter2 && self.copter3 ):
+				break
+			
+			# Copter 1's signal is 100, Copter 2's signal is 200, and Copter 3's signal is 300
+			if( coptersignal == 100 ):
+				self.copter1 = 1
+			elif( coptersignal == 200 ):
+				self.copter2 = 1
+			elif( coptersignal == 300 ):
+				self.copter3 = 1
 
 def init(mpstate):
 	"""initialize module"""
