e4bddab3f4cb8e9e0132eb1414ae87c0916d2deb
HerbertHoover <srich046@odu.edu>
2017-06-14 14:59:14 -0400

    chachachachachanges

--- module_testing_CN.py
+++ module_testing_CN.py
@@ -23,24 +23,12 @@ class CmdTestModule(mp_module.MPModule):
 	super(CmdlongModule, self).__init__(mpstate, "cmdlong")
 	"""Commads for CmdTestmodule"""
 
-	# Method to arm the three copters
-	# Doesn't work if you have less than 3 copters 
-	def armCopters(self, coptersingal):
-		# Set copters to a default of False or "off"
-		self.copter1 = 0, self.copter2 = 0, self.copter3 = 0
-		
-		while( True ):
-			# Stop once all signals have been received
-			if( self.copter1 and self.copter2 and self.copter3 ):
-				break
+	# Method to arm copter
+	def armCopters(self):
+		if( !Script.armed )
+			MavLink.doARM( True )
 			
-			# Copter 1's signal is 100, Copter 2's signal is 200, and Copter 3's signal is 300
-			if( coptersignal == 100 ):
-				self.copter1 = 1
-			elif( coptersignal == 200 ):
-				self.copter2 = 1
-			elif( coptersignal == 300 ):
-				self.copter3 = 1
+			
 
 def init(mpstate):
 	"""initialize module"""
