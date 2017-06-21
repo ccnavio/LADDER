ba7248e1ec2d598b15d2173ebf50b34deed79f9c
ccnavio <carie.navio@gmail.com>
2017-06-21 16:14:28 -0400

    adding master flight test. need to change valids for PWM and finish before flight tomorrow. Just a test

--- script_simple_hover_flight_master.py
+++ script_simple_hover_flight_master.py
@@ -1,7 +1,7 @@
 # Carie Navio
 # Mission Planner Script
 # Purpose: Waypoint script testing
-# THIS DOES NOT INCLUDE THE UNLINKING PORTION
+# THIS IS TESTING ON A QUAD
 
 # 1 roll
 # 2 pitch
@@ -13,7 +13,6 @@
 # 8 empty (?)
 # 9 epm activation 
 
-# safety switch on radio 7
 def Safety_Check():
 	if cs.ch7in > 1800:
 		for chan in range(1,9):
@@ -41,7 +40,8 @@ for chan in range (6,9):
 Script.Sleep(2000)
 print 'Copter should start arming'
 MAV.doARM(True)
-print 'Copter should be armed'
+
+print 'Copter should be armed'				
 Script.Sleep(2000)
 # When initialized, the copter is set to Stabilize
 # Throttle PWM values will change for our specific copter
@@ -57,7 +57,7 @@ while cs.alt < 5:
 	Script.Sleep(50)
 
 print 'Copter slowing to 4 m'
-Script.SendRC(3,1200,True)
+Script.SendRC(3,1150,True)
 while cs.alt < 4:
 	cs.verticalspeed = 0.25
 	Safety_Check()
@@ -71,13 +71,32 @@ while cs.alt < 5:
 											# it will maintain just under 5 m / 16 ft 
 print 'AltHold copter'
 Script.SendRC(5,1400,True)					# This should be AltHold
-Safety_Check()
-print 'Sleeping'
-Script.Sleep(5000)
+
+# EPM ON CHANNEL 9
+
+# Make sure we've stopped rising
+# This will last 1.5 seconds
+print 'Maintain position'
+wait_before_unlink = 0
+while wait_before_unlink < 30:
+	Safety_Check()
+	Script.Sleep(50)
+	wait_before_unlink = wait_before_unlink + 1
+
+# Unlinking will last for 0.5 seconds
+setting = 0
+print 'Unlinking'
+while setting < 10:
+	cs.ch9out = 1800
+	Safety_Check()
+	Script.Sleep(50)
+	setting = setting + 1
+cs.ch9out = 0
+Script.Sleep(4500)
 
 print 'Finished AltHold'
-
-Script.SendRC(3,1200,True)
+Script.ChangeMode("Stabilize")				# Return to stabilize mode
+Script.SendRC(3,1300,True)
 while cs.alt > 0.2:
 	Safety_Check()
 	Script.Sleep(50)
@@ -87,5 +106,5 @@ print 'Copter Disarmed'
 
 for chan in range(1,9):
 	Script.SendRC(chan,0,False)
-# --------------------------------------------- #
+
 print 'Script Over'
