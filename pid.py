a0013310edd39dda3a506a658e7307d669f9f248
HerbertHoover <srich046@odu.edu>
2017-07-17 10:05:48 -0400

    v1.3

--- pid.py
+++ pid.py
@@ -23,8 +23,8 @@ while error > 0.5
 	integral += error*dt					# Sum of error from setpoint
 	derivative = (error - previous_error)/dt		# How fast the error changes
 	output = Kp*error + Ki*integral + Kd*derivative		# Response of the system
-	if minimumThrust*abs(output) > 1700			# Is 1700 too much?
+	if minimumThrust+abs(output) > 1700		# Is 1700 too much?
 		continue
-	# SendRC(1:4, minimumThrust*abs(output), True) 
+	# SendRC(1:4, minimumThrust+abs(output), True) 
 	previous_error = error					# Reset previous error for derivative term next loop
 
