e6429e513c7425381a6e24974e6723b5ed1cc4f0
HerbertHoover <srich046@odu.edu>
2017-07-17 09:23:33 -0400

    pypid

---
+++ pid.py
@@ -0,0 +1,28 @@
+# PID 
+
+# Point you want to be at
+setpoint = 0
+
+# Error in the system
+error = 0
+
+# Time differential ( Set to 10 Hz )
+dt = 0.1
+
+# Measured value or what you read from sensors
+measured_value = 10
+
+# Gains
+Kp = 1
+Ki = 1
+Kd = 0.01
+
+# Control Loop
+while error > 0.5
+	error = setpoint - measured_value			# Difference between where you want to be from where you are
+	integral += error*dt					# Sum of error from setpoint
+	derivative = (error - previous_error)/dt		# How fast the error changes
+	output = Kp*error + Ki*integral + Kd*derivative		# Response of the system
+	# SendRC(1:4, minimumThrust*output, True) 
+	previous_error = error;					# Reset previous error for derivative term next loop
+
