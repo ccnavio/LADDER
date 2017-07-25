63c57e9a051f306e97433b1081f355b848c485ea
HerbertHoover <srich046@odu.edu>
2017-07-25 12:17:32 -0400

    updated longitude param name

--- pressureIO.py
+++ pressureIO.py
@@ -6,6 +6,6 @@ while os.path.exists( 'pressureReadings%s.txt' % i ):
 	i += 1
 
 with io.open( 'pressureReadings%s.txt' % i, 'w' ) as file:
-	data = str(cs.lat) + ' ' + str(cs.lon) + ' ' + str(cs.alt) + ' ' + str(cs.press_abs) + '\n' 
+	data = str(cs.lat) + ' ' + str(cs.lng) + ' ' + str(cs.alt) + ' ' + str(cs.press_abs) + '\n' 
 	file.write(u"%s" % data)
 	file.close()
