d18791ac939e0ce425aa719217c0392ebcbbdf9f
HerbertHoover <srich046@odu.edu>
2017-07-25 11:04:42 -0400

    Pressure IO
    Python code to incorporate into main script to log pressure values to their respective GPS coordinates.

---
+++ pressureIO.py
@@ -0,0 +1,11 @@
+import io
+import os
+
+i = 1
+while os.path.exists( 'pressureReadings%s.txt' % i ):
+	i += 1
+
+with io.open( 'pressureReadings%s.txt' % i, 'w' ) as file:
+	data = str(cs.lat) + ' ' + str(cs.lon) + ' ' + str(cs.alt) + ' ' + str(cs.press_abs) + '\n' 
+	file.write(u"%s" % data)
+	file.close()
