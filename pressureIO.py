8a02deacc5afdba6e187b7e7bd2ac3c681a0adf3
HerbertHoover <srich046@odu.edu>
2017-07-25 12:28:11 -0400

    oops

--- pressureIO.py
+++ pressureIO.py
@@ -6,6 +6,6 @@ while os.path.exists( 'pressureReadings%s.txt' % i ):
 	i += 1
 
 with io.open( 'pressureReadings%s.txt' % i, 'w' ) as file:
-	data = '%.2f' % cs.lat + ' ' + '%.2f' % cs.lng + ' ' + '%.2f' % cs.alt + ' ' + '%0.3f' % cs.press_abs 
+	data = '%.2f' % cs.lat + ' ' + '%.2f' % cs.lng + ' ' + '%.2f' % cs.alt + ' ' + '%.3f' % cs.press_abs 
 	file.write(u"%s\n" % data)
 	file.close()
