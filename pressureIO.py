a1e582d151c9ea068311c2e217d5712dbd52c9e2
HerbertHoover <srich046@odu.edu>
2017-07-25 12:27:48 -0400

    Updated float -> string

--- pressureIO.py
+++ pressureIO.py
@@ -6,6 +6,6 @@ while os.path.exists( 'pressureReadings%s.txt' % i ):
 	i += 1
 
 with io.open( 'pressureReadings%s.txt' % i, 'w' ) as file:
-	data = str(cs.lat) + ' ' + str(cs.lng) + ' ' + str(cs.alt) + ' ' + str(cs.press_abs) + '\n' 
-	file.write(u"%s" % data)
+	data = '%.2f' % cs.lat + ' ' + '%.2f' % cs.lng + ' ' + '%.2f' % cs.alt + ' ' + '%0.3f' % cs.press_abs 
+	file.write(u"%s\n" % data)
 	file.close()
