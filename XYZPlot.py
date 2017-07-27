8f9d23b21a9685aa2925a3314f03bb07571a28c0
JacobAlanGoldsberry <jagoldsberry@wpi.edu>
2017-07-27 17:32:14 -0400

    XYZPlot.py
    Plotting values from three different files into a 3D model.

--- XYZPlot.py
+++ XYZPlot.py
@@ -1,54 +1,43 @@
+#JacobAlanGoldsberry
 #Work with matplotlib
+#Plotting X,Y, and Z coordinates in 3 dimensions
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+# def strip(c):
+#     c = [x.strip() for x in c]
 
 #Create Data
 path = "C:/Users/jgoldsb1/Desktop/"
 file_name = "potato"
 file_name2 = "broccoli"
+file_name3 = "asparagus"
 R = open(path+file_name+'.txt', "r")
 RR = open(path+file_name2+'.txt', "r")
-z = R.readlines()
-z = [x.strip() for x in z]
+RRR = open(path+file_name3+'.txt', "r")
+w = R.readlines()
+w = [float(x.strip()) for x in w]
 y = RR.readlines()
-y = [x.strip() for x in y]
+y = [float(x.strip()) for x in y]
+z = RRR.readlines()
+z = [float(x.strip()) for x in z]
 
-print z
+print w
+print len(w)
 print y
+print len(y)
+print z
+print len(z)
 
-# colors = (1,0,0)
-# area = np.pi*3
-
-# Plot
-# plt.scatter(z, y, s=area, c=colors, alpha=0.5)
-# plt.axis([-10, 10, -10, 10])
-# plt.grid(True)
-# plt.title('Estimated travel from zero')
-# plt.xlabel('x (meters)')
-# plt.ylabel('y (meters)')
-# plt.show()
-
-#Scatterplot code from matplotlib
-def randrange(n, vmin, vmax):
-    '''
-    Helper function to make an array of random numbers having shape (n, )
-    with each number distributed Uniform(vmin, vmax).
-    '''
-    return (vmax - vmin)*np.random.rand(n) + vmin
+#3D plot
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-n = 100
-
-# For each set of style and range settings, plot n random points in the box
-# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
 for c, m, zlow, zhigh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
-    xs = randrange(n, 23, 32)
-    ys = randrange(n, 0, 100)
-    zs = randrange(n, zlow, zhigh)
-    ax.scatter(xs, ys, zs, c=c, marker=m)
+
+    ax.plot(w, y, z, c=c, marker=m)
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
